384c58b6f9d7a2d914d5605b27ce95e6d0e19c78
the samurai guy <118777814+samarin008@users.noreply.github.com>
2023-01-14 11:21:42 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 function switchtheme() {
-    
+    currentimg = document.getElementById("themeimg").src;
+    concatimg = currentimg.substring(22,29);
+    console.log(concatimg);
+    var element = document.body;
+    element.classList.toggle("dark-mode")
+    if (concatimg == "lmi.png") {
+        document.getElementById("themeimg").src="dmi.svg";
+    }
+    else if (concatimg == "dmi.svg") {
+        document.getElementById("themeimg").src="lmi.png";
+    }
 }
